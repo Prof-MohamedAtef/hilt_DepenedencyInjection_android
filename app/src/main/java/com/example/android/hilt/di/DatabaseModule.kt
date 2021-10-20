71c8c2e8156dd6a8260063afd5ef5201d45cacb8
Prof-MohamedAtef <proofesser@gmail.com>
2021-10-20 05:29:54 +0200

    publish changes

---
+++ app/src/main/java/com/example/android/hilt/di/DatabaseModule.kt
@@ -0,0 +1,19 @@
+package com.example.android.hilt.di
+
+import com.example.android.hilt.data.AppDatabase
+import com.example.android.hilt.data.LogDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class DatabaseModule {
+
+    @Provides
+    fun provideLogDao(database: AppDatabase): LogDao {
+        return database.logDao()
+    }
+}
